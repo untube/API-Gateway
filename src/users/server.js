233a8c4b9bc70c97971911fd61366f6c25b80aef
Bryan Antonio Angarita Rodriguez <baangaritar@unal.edu.co>
2019-06-03 14:02:16 -0500

    Api users again

--- src/users/server.js
+++ src/users/server.js
@@ -1,3 +1,3 @@
 export const url = process.env.USERS_SIGNUP_URL
-export const port = process.env.USERS_SIGNUP_PORT
-export const entryPoint = process.env.USERS_SIGNUP_ENTRY
+export const port = process.env.USERS_SIGNUP_PORT 
+export const entryPoint = process.env.USERS_SIGNUP_ENTRY 
--- src/users/typeDefs.js
+++ src/users/typeDefs.js
@@ -12,25 +12,39 @@ type User {
     updated_at: String!
 }
 
+type LoginResponse {
+    id: Int!
+    email: String!
+    name: String!
+    nickname: String!
+
+}
+
 input UserInput {
     email: String!
+    name: String!
+    nickname: String!
     password: String!
     password_confirmation: String!
     
+}
+
+input UserLogin {
+    email: String!
+    password: String!
 }`;
 
 export const usersQueries = `
-    allUsers: [User]!
-    usersById(id: String!): User!
-    usersByEmail(email: String!): String! 
-    usersByName(name: String!): String! 
-    usersByNickname(nickname: String!): String!   
+    userById(id: String!): User
+    userByEmail(email: String!): User 
+    userByName(name: String!): User
+    userByNickname(nickname: String!): User   
 `;
 
 export const usersMutations = `
-    createUser(user: UserInput!): User!
-    deleteUser(id: String!): String
-    updateUser(id: String!, user: UserInput!): User!
-    loginUser(email: String!, password: String!): User!
+    createUser(user: UserInput!): User
+    deleteUser(id: Int!): User
+    updateUser(id: Int!, user: UserInput!): User
+    loginUser(id: Int!, user: UserLogin): LoginResponse
 
 `;
