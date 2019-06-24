26eed29398f04b7d20f537887c2a22c6207c6ffe
baangaritar <baangaritar@unal.edu.co>
2019-06-24 10:20:34 -0500

    Config authentication

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -5,41 +5,48 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { mergeSchemas } from './utilities';
 
 import {
-	videosMutations,
-	videosQueries,
-	videosTypeDef,
+    videosMutations,
+    videosQueries,
+    videosTypeDef,
 } from './videos/typeDefs';
 
 import {
-	categoriesMutations,
-	categoriesQueries,
-	categoriesTypeDef,
-}from './categories/typeDefs';
+    categoriesMutations,
+    categoriesQueries,
+    categoriesTypeDef,
+} from './categories/typeDefs';
 
-
-import {commentariesMutations,
-	commentariesQueries,
-	commentariesTypeDef,
-}from './commentaries/typeDefs';
-
-import{
-	recommendationsMutations,
-	recommendationsQueries,
-	userPreferencesTypeDef,
-	videosStatisticsTypeDef,
-}from './recommendations/typeDefs'
 
 import {
-	videosulMutations,
-	videosulQueries,
-	videosulTypeDef,
+    commentariesMutations,
+    commentariesQueries,
+    commentariesTypeDef,
+} from './commentaries/typeDefs';
+
+import {
+    recommendationsMutations,
+    recommendationsQueries,
+    userPreferencesTypeDef,
+    videosStatisticsTypeDef,
+} from './recommendations/typeDefs'
+
+import {
+    videosulMutations,
+    videosulQueries,
+    videosulTypeDef,
 } from './videosul/typeDefs';
 
 import {
-	usersMutations,
-	usersQueries,
-	usersTypeDef,
+    usersMutations,
+    usersQueries,
+    usersTypeDef,
 } from './users/typeDefs';
+
+import {
+    authMutations,
+    authQueries,
+    authTypeDef
+} from './authentication/typeDefs';
 
 
 
@@ -48,49 +55,51 @@ import categoriesResolvers from './categories/resolvers';
 import commentariesResolvers from './commentaries/resolvers';
 import videosulResolvers from './videosul/resolvers';
 import recommendationsResolvers from './recommendations/resolvers';
-import usersResolvers from './users/resolvers'
+import usersResolvers from './users/resolvers';
+import authResolvers from './authentication/resolvers';
 
 
 // merge the typeDefs
 const mergedTypeDefs = mergeSchemas(
-	[
-		'scalar JSON',
-		videosTypeDef,
-		commentariesTypeDef,
-		videosulTypeDef,
-		categoriesTypeDef,
-		userPreferencesTypeDef,
-		videosStatisticsTypeDef,
-		usersTypeDef
-	],
-	[
-		videosQueries,
-		categoriesQueries,
-		commentariesQueries,
-		videosulQueries,
-		recommendationsQueries,
-		usersQueries
-	],
-	[
-		videosMutations,
-		commentariesMutations,
-		videosulMutations,
-		categoriesMutations,
-		recommendationsMutations,
-		usersMutations
-	]
+    [
+        'scalar JSON',
+        videosTypeDef,
+        commentariesTypeDef,
+        videosulTypeDef,
+        categoriesTypeDef,
+        userPreferencesTypeDef,
+        videosStatisticsTypeDef,
+        usersTypeDef,
+        authTypeDef
+    ], [
+        videosQueries,
+        categoriesQueries,
+        commentariesQueries,
+        videosulQueries,
+        recommendationsQueries,
+        usersQueries,
+        authQueries
+    ], [
+        videosMutations,
+        commentariesMutations,
+        videosulMutations,
+        categoriesMutations,
+        recommendationsMutations,
+        usersMutations,
+        authMutations
+    ]
 );
 
 // Generate the schema object from your types definition.
 export default makeExecutableSchema({
-	typeDefs: mergedTypeDefs,
-	resolvers: merge(
-		{ JSON: GraphQLJSON }, // allows scalar JSON
-		videosResolvers,
-		commentariesResolvers,
-		videosulResolvers,
-		categoriesResolvers,
-		recommendationsResolvers,
-		usersResolvers
-	)
+    typeDefs: mergedTypeDefs,
+    resolvers: merge({ JSON: GraphQLJSON }, // allows scalar JSON
+        videosResolvers,
+        commentariesResolvers,
+        videosulResolvers,
+        categoriesResolvers,
+        recommendationsResolvers,
+        usersResolvers,
+        authResolvers
+    )
 });
