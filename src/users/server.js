1bdbec7b54774f73ed43b674bdb9333d8006b6de
baangaritar <baangaritar@unal.edu.co>
2019-06-24 13:49:15 -0500

    Port drop

--- src/users/server.js
+++ src/users/server.js
@@ -1,3 +1,3 @@
 export const url = process.env.AUTHENTICATION_URL || 'authentication-ms';
-export const port = process.env.AUTHENTICATION_PORT || '3000';
+export const port = process.env.AUTHENTICATION_PORT;
 export const entryPoint = process.env.AUTHENTICATION_ENTRY || 'auth';
