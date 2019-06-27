3faca6f5892302a54c32b24f404e94cfde46d10e
Diego Araque <daaraquem@unal.edu.co>
2019-06-27 03:57:42 -0500

    env variables recommendations server

--- src/recommendations/server.js
+++ src/recommendations/server.js
@@ -2,6 +2,6 @@ export const url = process.env.RECOMMENDATIONS_URL || 'recommendations-ms'
 export const port = process.env.RECOMMENDATIONS_PORT || '3003'
 export const entryPoint = process.env.RECOMMENDATIONS_ENTRY || 'recommendations'
 
-export const urlv = process.env.REPRODUCTION_URL
-export const portv = process.env.REPRODUCTION_PORT
-export const entryPointv = process.env.REPRODUCTION_ENTRY
+export const urlv = process.env.REPRODUCTION_URL || 'reproduction-ms'
+export const portv = process.env.REPRODUCTION_PORT || '3002'
+export const entryPointv = process.env.REPRODUCTION_ENTRY || 'videos'
