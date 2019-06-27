db7e97f56471c0963884d29ecfb091931b40bcbc
Diego Araque <daaraquem@unal.edu.co>
2019-06-26 19:34:17 -0500

    Recommendations query

--- src/recommendations/server.js
+++ src/recommendations/server.js
@@ -1,3 +1,7 @@
 export const url = process.env.RECOMMENDATIONS_URL
 export const port = process.env.RECOMMENDATIONS_PORT
 export const entryPoint = process.env.RECOMMENDATIONS_ENTRY
+
+export const urlv = process.env.REPRODUCTION_URL
+export const portv = process.env.REPRODUCTION_PORT
+export const entryPointv = process.env.REPRODUCTION_ENTRY
