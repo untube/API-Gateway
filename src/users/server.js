a3c955418586cb2a9b2945f594b3c8216d7ff874
Jose Caviedes <hjcaviedesb@unal.edu.co>
2019-07-15 22:08:14 -0500

    Port fixes

--- src/users/server.js
+++ src/users/server.js
@@ -1,6 +1,6 @@
 //export const url = process.env.USERS_URL || 'authentication-ms';
 //export const port = process.env.USERS_PORT || '3000';
-export const url = 'authentication-lb'
-export const url = '4007'
+export const url =  process.env.USERS_URL || 'authentication-lb'
+export const port =  process.env.USERS_PORT || '4007'
 export const entryPoint = process.env.USERS_ENTRY || 'auth';
 
--- src/videosul/server.js
+++ src/videosul/server.js
@@ -1,6 +1,6 @@
 //export const url = process.env.VIDEOSUL_URL || 'manager-ms'
 //export const port = process.env.VIDEOSUL_PORT || '3001'
 
-export const url = 'manager-lb'
-export const url = '4005'
+export const url =  process.env.VIDEOSUL_URL || 'manager-lb'
+export const port = process.env.VIDEOSUL_PORT || '4006'
 export const entryPoint = process.env.VIDEOSUL_ENTRY || 'uploadVideos'
